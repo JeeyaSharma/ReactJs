89342f33b573cb475b7bc9538a3ed5819590b865
JeeyaSharma <jeeyas39@gmail.com>
2024-10-29 02:00:07 +0530

    Started Basics of React

---
+++ 1st App/firstReactApp/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <ul>
+                <li>Terms&Conditions</li>
+                <li>Feedback</li>
+                <li>FAQs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
---
+++ 1st App/firstReactApp/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Navbar = () => {
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Navbar 
